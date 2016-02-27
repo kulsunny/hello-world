b8927d0a57ca61ecc9e8c54b8e78bf94bdae607d
Gaurav Sharma <gauravsharma81189@gmail.com>
2016-02-28 00:01:21 +1100

    Create find_odd_occuring_element.py

---
+++ DataStructures/Array/find_odd_occuring_element.py
@@ -0,0 +1,12 @@
+# Given an array which contains all elements occuring even number of times except one
+# WAF to find that element
+
+def find_odd_occuring_element(arr):
+	x = arr[0]
+	for i in xrange(len(arr)-1):
+		x = x^arr[i+1]
+	return x
+
+if __name__ == '__main__':
+	arr = [1,2,1,3,2,3,2,2,3,3,4,5,4]
+	print 'Odd occuring element is', find_odd_occuring_element(arr)
